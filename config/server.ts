a8a3e815d8711d53dff5b4130d25ee508678540d
simone <simone.l.silva@parceiros.nos.pt>
2025-04-03 14:20:50 +0100

    chore: preparing to prd

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,14 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', ''),
   app: {
     keys: env.array('APP_KEYS'),
+  },
+  admin: {
+    url: '/admin',
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'default_secret'),
+    },
   },
 });
